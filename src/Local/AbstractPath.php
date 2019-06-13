128414129e82e52df1df1d6391ebfebb20fd26b1
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-13 10:11:34 +0500

    updates

--- src/Local/AbstractPath.php
+++ src/Local/AbstractPath.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace Comely\Filesystem\Local;
 
+use Comely\Filesystem\Directory;
 use Comely\Filesystem\Exception\PathException;
 
 /**
@@ -22,11 +23,24 @@ use Comely\Filesystem\Exception\PathException;
  */
 abstract class AbstractPath
 {
+    public const IS_DIRECTORY = 0x64;
+    public const IS_FILE = 0xc8;
+    public const IS_LINK = 0x012c;
+
     /** @var string */
     private $path;
+    /** @var null|int */
+    private $type;
     /** @var null|Permissions */
     private $permissions;
+    /** @var null|bool */
+    private $deleted;
 
+    /**
+     * AbstractPath constructor.
+     * @param string $path
+     * @throws PathException
+     */
     public function __construct(string $path)
     {
         $path = realpath($path); // Get an absolute real path
@@ -34,24 +48,45 @@ abstract class AbstractPath
             throw new PathException('File or directory does not exist');
         }
 
+        // Type
+        if (is_dir($path)) {
+            $this->type = self::IS_DIRECTORY;
+        } elseif (is_file($path)) {
+            $this->type = self::IS_FILE;
+        } elseif (is_link($path)) {
+            $this->type = self::IS_LINK;
+        }
+    }
 
+    /**
+     * @return int|null
+     */
+    public function type(): ?int
+    {
+        return $this->type;
     }
 
     /**
      * @return AbstractPath
+     * @throws PathException
      */
     public function clearStatCache(): self
     {
-        clearstatcache(true, $this->path);
+        clearstatcache(true, $this->path());
         $this->permissions()->reset(); // Reset cached permissions
         return $this;
     }
 
     /**
      * @return string
+     * @throws PathException
      */
     public function path(): string
     {
+        if ($this->deleted) {
+            throw new PathException('File/directory path has been deleted is no longer valid');
+        }
+
         return $this->path;
     }
 
@@ -65,5 +100,19 @@ abstract class AbstractPath
         }
 
         return $this->permissions;
+    }
+
+    /**
+     * @return Directory
+     * @throws PathException
+     */
+    public function parent(): Directory
+    {
+        return new Directory(dirname($this->path));
+    }
+
+    public function delete(bool $recursive = false): void
+    {
+
     }
 }
