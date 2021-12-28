4cce48b4f9a3aaf085a2e9d55e2155e0b6935f48
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-12-28 06:22:13 +0500

    changes preg modifier char

--- src/Directory.php
+++ src/Directory.php
@@ -51,10 +51,10 @@ class Directory extends AbstractPath
     public function suffix(string $path): string
     {
         $sep = '([/\\\])';
-        $path = preg_replace('/' . $sep . '{2,}/', DIRECTORY_SEPARATOR, $path);
-        if (!preg_match('/^(' . $sep . '?[\w\-.]+' . $sep . '?)+$/', $path)) {
+        $path = preg_replace('#' . $sep . '{2,}#', DIRECTORY_SEPARATOR, $path);
+        if (!preg_match('#^(' . $sep . '?[\w\-.]+' . $sep . '?)+$#', $path)) {
             throw new \InvalidArgumentException('Invalid suffix path');
-        } elseif (preg_match('/(\.+' . $sep . ')/', $path)) {
+        } elseif (preg_match('#(\.+' . $sep . ')#', $path)) {
             throw new \InvalidArgumentException('Path contains illegal references');
         }
 
